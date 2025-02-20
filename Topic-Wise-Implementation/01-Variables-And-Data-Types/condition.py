47a516e3b36509333ce5c422d920982d1db6ee14
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-20 22:54:11 +0500

    Updationc

--- Topic-Wise-Implementation/01-Variables-And-Data-Types/condition.py
+++ Topic-Wise-Implementation/01-Variables-And-Data-Types/condition.py
@@ -1,4 +1,6 @@
+# Takes Two Number Input From User
 first = int(input("Enter Number 1: "))
 second = int(input("Enter Number 2: "))
 
+# Print True or False According To The Condition If First is greater then or equal to second Number it prints True otherwise False
 print(first >= second)
--- Topic-Wise-Implementation/01-Variables-And-Data-Types/sum.py
+++ Topic-Wise-Implementation/01-Variables-And-Data-Types/sum.py
@@ -1,6 +1,8 @@
+# Takes Two Number Input From The User
 first = int(input("Enter Number 1: "))
 second = int(input("Enter Number 2: "))
 
+# Print The Sum Of Two Number 
 print("The Sum Of", first, "and", second, "is", first + second)
 
 # For Average
