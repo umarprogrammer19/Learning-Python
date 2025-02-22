d23a6e684b34de8aa998707783043eaaedefac0c
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-22 18:16:29 +0500

    str Questions

---
+++ Topic-Wise-Implementation/02-String-And-Conditional-Statements/str_ques.py
@@ -0,0 +1,8 @@
+# WAP (write a program to input user's first name and print its length);
+first_name = input("Enter Your First Name: ")
+print(len(first_name))
+
+# WAP to count a number of given words in a string
+str = input("Enter anythings: ")
+word = input("Enter a word to count: ")
+print("The number of",word,"in the string is",str.count(word))
