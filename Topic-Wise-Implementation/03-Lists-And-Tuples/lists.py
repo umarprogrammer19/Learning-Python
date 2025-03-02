b9e6a6d0fc230a5c344f7abd37195bce7189d157
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-03 00:38:14 +0500

    Lists

--- Topic-Wise-Implementation/03-Lists-And-Tuples/lists.py
+++ Topic-Wise-Implementation/03-Lists-And-Tuples/lists.py
@@ -4,20 +4,21 @@ marks = [10, 20, 30, 40, 50, 60, 70]
 # For printing Type
 print(type(marks))  # list
 
-# Print All Values in the list 
+# Print All Values in the list
 print(marks)
 
 # Prints a single value by using index
 print(marks[0])  # 10
 
-# For finding the length 
+# For finding the length
 print(len(marks))  # 7
 
-# Changing of values from index is allowed in lists 
+# Changing of values from index is allowed in lists
 marks[6] = 80
 
-print(marks) # prints 80 intead of 70 on index 6
+print(marks)  # prints 80 intead of 70 on index 6
 
-print(marks[7]) # Gives IndexError: list index out of range
+# print(marks[7]) # Gives IndexError: list index out of range
 
+print(marks[3 : len(marks)])  # prints from index 3 till last
 
