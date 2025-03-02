2a8d7d3a00c53b853cc447f4149be7586d5876c9
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-03 01:17:15 +0500

    Methods

--- Topic-Wise-Implementation/03-Lists-And-Tuples/methods_lists.py
+++ Topic-Wise-Implementation/03-Lists-And-Tuples/methods_lists.py
@@ -13,3 +13,23 @@ print(a)  # [1, 2, 3, 5, 7, 8, 9, 10]
 
 a.sort(reverse=True)
 print(a)  # [10, 9, 8, 7, 5, 3, 2, 1]
+
+# 3. reverse() reverse the lists
+lists.reverse()
+print(lists)  # [6, 5, 4, 3, 2, 1]
+
+# 4. remove() it removes the 1st given element of the list for example if there is umar two times in the lists ao it is removes 1st umar coming in the list
+lists.remove(4)
+print(lists)  # [6, 5, 3, 2, 1]
+
+# 5. insert it is used for inserting any value at any index
+lists.insert(5, 10)  # add 10 in 5th index
+print(lists)  # [6, 5, 3, 2, 1, 10]
+
+# 6. pop() it removes element from the given index in the list
+lists.pop(2)
+print(lists)  # [6, 5, 2, 1, 10]
+
+# 7. clear() removes all element from the lists
+lists.clear()
+print(lists) # []
