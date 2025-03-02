e013934fbf1f38878c305c8bf03cafe00e83496c
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-02 19:37:15 +0500

    Conditionals

---
+++ Topic-Wise-Implementation/02-String-And-Conditional-Statements/conditionals.py
@@ -0,0 +1,20 @@
+age = 17
+
+# if checks the condition is true it will execute if statement otherwise else statement
+if age >= 18:
+    print("You are eligible for license")
+else:
+    print("You are not eligible for license")
+
+
+# elif for multiple conditions
+light = "green"
+
+if light == "red":
+    print("Stop")
+elif light == "yellow":
+    print("Ready")
+elif light == "green":
+    print("go")
+else:
+    print("Invalid Traffic Light")
--- Topic-Wise-Implementation/02-String-And-Conditional-Statements/str_ques.py
+++ Topic-Wise-Implementation/02-String-And-Conditional-Statements/str_ques.py
@@ -1,4 +1,4 @@
-# WAP (write a program to input user's first name and print its length);
+# WAP (write a program ) to input user's first name and print its length;
 first_name = input("Enter Your First Name: ")
 print(len(first_name))
 
