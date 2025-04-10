66720e8b10002900abf79884ab47503eefbd1507
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-04-10 06:56:37 +0500

    Tuples Questions

---
+++ Topic-Wise-Implementation/04-Tuples/tuples_ques.py
@@ -0,0 +1,10 @@
+# WAP to count the number of students with the "A" grade in the followinf tupple
+# ("C", "D", "A", "A", "B", "B", "A")
+
+tup = ("C", "D", "A", "A", "B", "B", "A")
+print(tup.count("A"))  # 3
+
+# Store the above values in a list and sort them from A to D
+list = ["C", "D", "A", "A", "B", "B", "A"]
+list.sort()
+print(list)
