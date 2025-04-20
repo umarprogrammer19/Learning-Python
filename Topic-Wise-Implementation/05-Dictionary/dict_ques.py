6b82157c6969362555be7fd4f81fa68b1091eba3
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-04-20 22:57:10 +0500

    Q2 Dict

--- Topic-Wise-Implementation/05-Dictionary/dict_ques.py
+++ Topic-Wise-Implementation/05-Dictionary/dict_ques.py
@@ -10,3 +10,11 @@ meanings = (
 )
 
 print(meanings)
+
+# WAP to enter the marks of 3 subjects from the user and store them in a dictionary. Start with an empty dictionary & add one by one, Use Subjects name as keys & marks as value
+
+marks_dict = {}
+marks_dict.update({"maths": int(input("Please Enter Your Maths Number: ").strip())})
+marks_dict.update({"physics": int(input("Please Enter Your Physics Number: ").strip())})
+marks_dict.update({"chemistry": int(input("Please Enter Your Chemisty Number: ").strip())})
+print(marks_dict)
