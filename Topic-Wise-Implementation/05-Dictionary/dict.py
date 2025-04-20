8d7aa0e2eb951cf4274e259088e0e3831cff22ea
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-04-20 19:56:59 +0500

    Dictionary

---
+++ Topic-Wise-Implementation/05-Dictionary/dict.py
@@ -0,0 +1,15 @@
+# Store Data In Key Value Pair
+user = {
+    "name": "Umar Farooq",
+    "age": 17,
+    "isAlive": True,
+    "subjects": ["Javascript", "Typescript", "Python"],
+}
+
+# For Printing Simple One Value
+print(user["name"])
+print(user["age"])
+print(user["isAlive"])
+print(user["subjects"])
+
+# For Changing Values 
