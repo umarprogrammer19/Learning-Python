44c2d2ca0ccb479d9b127760c6675c655782905e
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-04-20 22:48:27 +0500

    set Ques

---
+++ Topic-Wise-Implementation/06-Sets/sets_ques.py
@@ -0,0 +1,17 @@
+# you are given a list of subjects. Assume one classroom in required for one subject how many classrooms are needed by all students
+# "python", "java","C++","python","javascript","java","python","java","C++","C"
+
+subjects = {
+    "python",
+    "java",
+    "C++",
+    "python",
+    "javascript",
+    "java",
+    "python",
+    "java",
+    "C++",
+    "C",
+}
+
+print(f"{len(subjects)} classrooms are needed by all students")
