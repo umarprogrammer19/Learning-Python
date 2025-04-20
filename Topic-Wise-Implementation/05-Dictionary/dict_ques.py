6ed212582264678f21e86bb89a3d9bec67e23d6a
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-04-20 22:44:57 +0500

    Questions Of Dictionary

---
+++ Topic-Wise-Implementation/05-Dictionary/dict_ques.py
@@ -0,0 +1,12 @@
+# store the following meanings in the dictionary
+# table: "a peice of furniture", "list of fact & figures"
+# cat: "a small animal"
+
+meanings = (
+    {
+        "table": ["a perice of a furnitue", "list of fact & figure"],
+        "cat": "a small animal",
+    },
+)
+
+print(meanings)
