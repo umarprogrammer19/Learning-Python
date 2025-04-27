ddd004eeca40d8f63e0481493b7788c051e9322c
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-04-27 18:25:10 +0500

    Update

--- Topic-Wise-Implementation/10-OOPS-(Object-Oriented-Programming)/advance_oop.py
+++ Topic-Wise-Implementation/10-OOPS-(Object-Oriented-Programming)/advance_oop.py
@@ -32,3 +32,48 @@ class Cat(Animal):
 
 Dog("Dog", "Dog").speak()
 Cat("Cat", "Cat").speak()
+
+
+# Define a class Shape with a method area():
+
+# This method should be abstract (not implemented).
+# Create subclasses for Circle, Square, and Triangle.
+# Implement the area() method in each subclass.
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius**2
+
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side**2
+
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5 * self.base * self.height
+
+
+print(Circle(5).area())
+print(Square(5).area())
+print(Triangle(5, 5).area())
